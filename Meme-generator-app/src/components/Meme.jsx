631a3301e07aa7101a12e68b8e477ada5f50165f
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-28 09:28:38 +0200

    Removed the form and used a div instead

--- Meme-generator-app/src/components/Meme.jsx
+++ Meme-generator-app/src/components/Meme.jsx
@@ -3,31 +3,23 @@ import React from "react"
 export default function Meme() {
     return (
         <main>
-            <form className="form">
-                <div>
-                    <label htmlFor="top-text">Top Text</label>
-                    <input
-                        id="top-text"
-                        type="text"
-                        placeholder="Shut up"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <label>Bottom Text
-                    <input
-                            type="text"
-                            placeholder="and take my money"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <button
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
                     className="form--button"
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
