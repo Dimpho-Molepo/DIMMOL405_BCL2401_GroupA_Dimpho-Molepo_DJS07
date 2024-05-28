ac74adb3c8f405dce74fb9fadd6b31f7b83a4e00
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-28 17:44:53 +0200

    Added a state and getMemeImage to update the state and fetch a new meme image

--- Meme-generator-app/src/components/Meme.jsx
+++ Meme-generator-app/src/components/Meme.jsx
@@ -1,6 +1,26 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
     return (
         <main>
             <div className="form">
@@ -20,6 +40,7 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
